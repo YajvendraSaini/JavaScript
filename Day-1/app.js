324bec0965184ccd8f8d9b5333310c4e1740788d
Yajvendra Saini <yajvendra98@gmail.com>
2022-09-01 14:00:45 +0530

    Day-1 & Day-2 of learnig of JavaScript

---
+++ Day-1/app.js
@@ -0,0 +1,6 @@
+let msg = "Hi, this is my first code in JS";
+let num = "45";
+alert(msg);
+alert(num);
+num = "15";
+alert(num);
---
+++ Day-2/app.js
@@ -0,0 +1,10 @@
+let names = ['Nikky', 'Rohan', 'Varun', 'Ajay'];
+let job ={
+    title:'SDE',
+    place:'Karauli',
+    age: 20,
+}
+names.push("Yajvendra")
+alert(names[0]); 
+alert(names);
+alert(job.place);
