77ea5694bfc9b373292559cba5ea7d6f8506743c
Yajvendra Saini <yajvendra98@gmail.com>
2022-09-07 14:38:57 +0530

    Day-8 of learning JavaScript

---
+++ Day-8/app.js
@@ -0,0 +1,10 @@
+// random number generation in js
+
+// let a = Math.random();
+// alert(a);
+
+let x = prompt("Enter your name");
+let y = prompt("Enter your loved one name");
+let z = Math.random();
+    z = (z*100)+1;
+alert("Your love percentage " + z.toFixed(0) + "%");
