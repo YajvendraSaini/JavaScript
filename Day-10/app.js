2321b53ab0969a060578bf0892a419ab3cda8a6b
Yajvendra Saini <yajvendra98@gmail.com>
2022-09-09 18:07:28 +0530

    added name using push method

--- Day-10/app.js
+++ Day-10/app.js
@@ -1,8 +1,15 @@
+//working with arrey in JS
+
 let guestList = ["Rohan", "Sohan", "Mohan", "Raman", "Lalu", "Krishna"];
+
+// arrey.push();
+
+guestList.push("Nikky");
+
 let guestName = prompt("Enter your name");
+
 if (guestList.includes(guestName)) {
     alert("you are invited to the party 🥳");
 } else {
     alert("Sorry, you are not invited to the party 💔");
 }
-
