b892b66ad958dd528f0c4f4f732c5abab2f47a03
Yajvendra Saini <yajvendra98@gmail.com>
2022-09-11 15:07:38 +0530

    Day-12 of learning JavaScript

---
+++ Day-12/app.js
@@ -0,0 +1,10 @@
+// who's buying lunch
+
+function whopaying (names){
+
+    let totelPerson = person.lenght;
+    let rendomPersonPosition = Math.floor(Math.person()*totelPerson);
+    let rendomPerson = person[rendomPersonPosition];
+
+    alert(rendomPerson + " is going to but lunch today") ;
+}
